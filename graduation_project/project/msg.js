d4741dddfbd079e2cf59fe43d7086034d64c9b2c
Simone Catapano <58214844+SimDeveloper-cripto@users.noreply.github.com>
2021-07-05 21:10:01 +0200

    Delete graduation_project/project directory

--- graduation_project/project/msg.js
+++
@@ -1,5 +0,0 @@
-const frase = "Ora hai libero accesso ad alcuni dei tanti codici che ho creato. Fanne buon uso! ";
-const spazio = "<br> <br>";
-const frase2 = "Clicca qui per effettuare il <a href='logout.php'> LOGOUT </a>";
-
-document.write(frase + spazio + frase2);  
